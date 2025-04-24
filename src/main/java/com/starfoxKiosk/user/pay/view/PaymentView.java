bf39fe7cd48edaf7b718ab403140ea046c24b8af
jieun <yudaeun222@gmail.com>
2025-04-24 15:14:40 +0900

    pay

--- src/main/java/com/starfoxKiosk/user/pay/view/PaymentView.java
+++ src/main/java/com/starfoxKiosk/user/pay/view/PaymentView.java
@@ -1,7 +1,32 @@
 package com.starfoxKiosk.user.pay.view;
 
+import java.util.Scanner;
+
 public class PaymentView {
+
+    private final Scanner sc = new Scanner(System.in);
+
     public String selectPaymentMethod() {
+        System.out.println("""
+                결제 수단을 선택하세요. 
+                1. 카드
+                2. 간편결제
+                3. 삼성페이
+                """);
+
+        int choice = sc.nextInt();
+        switch (choice) {
+            case 1:
+                return "카드";
+            case 2:
+                return "간편결제";
+            case 3:
+            default:
+                System.out.println("잘못된 선택입니다. 기본값 '카드'로 진행합니다.");
+                return "카드";
+        }
+
+
     }
 
     public void showResult(boolean b) {
