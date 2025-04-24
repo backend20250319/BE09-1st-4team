5cb1d65be6c9852e48d916a048fbe7f3c227653f
binipk <mingyoku93@gmail.com>
2025-04-24 11:52:58 +0900

    Admin feature update

--- src/main/java/com/starfoxKiosk/admin/controller/OrderManagementController.java
+++
@@ -1,6 +0,0 @@
-package com.starfoxKiosk.admin.controller;
-
-public class OrderManagementController {
-
-
-}
---
+++ src/main/java/com/starfoxKiosk/admin/model/dto/Menu.java
@@ -0,0 +1,58 @@
+package com.starfoxKiosk.admin.model.dto;
+
+public class Menu {
+    private int id;
+    private String name;
+    private int price;
+    private int categoryId;
+
+    public Menu(int id, String name, int price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public Menu(int id, String name, int price, int categoryId) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.categoryId = categoryId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("ID: %d, 이름: %s, 가격: %d, 카테고리 ID: %d", id, name, price, categoryId);
+    }
+}
